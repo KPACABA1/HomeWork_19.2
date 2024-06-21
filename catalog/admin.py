13db8a81ed97038a4dcead213c0b863595819ea7
groni <smirnovapolina8200@mail.ru>
2024-06-22 04:53:05 +0500

    Новая миграция исправляющая ошибку загрузки моделей в админке

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,11 +4,14 @@ from django.contrib import admin
 from catalog.models import Product, Category
 
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'purchase_price', 'category')
-
-
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'name',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'purchase_price', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
