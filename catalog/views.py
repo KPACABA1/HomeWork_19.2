37e7589d3be597387b6c6ebc64af765aa69285b5
groni <smirnovapolina8200@mail.ru>
2024-06-28 01:37:38 +0500

    Всё готово

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'index.html', context)
 
 
 def contacts(request):
@@ -13,3 +17,8 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'Имя -{name}, телефон - {phone}, сообщение - {message}')
     return render(request, 'contacts.html')
+
+def info_product(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'info_product.html', context)
