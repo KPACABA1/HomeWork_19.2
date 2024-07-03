c09c44ea57c2bee07288b641e9678cf433ae1158
groni <smirnovapolina8200@mail.ru>
2024-07-03 20:17:36 +0500

    Ошибка при создании CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,12 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import index, contacts, info_product
+from catalog.views import IndexListView, contacts, info_product
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', IndexListView.as_view(), name='index'),
     path('contacts/', contacts, name='contacts'),
     path('info_product/<int:pk>/', info_product, name='info_product'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from catalog.models import Product
 
 
 # Create your views here.
-def index(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, 'index.html', context)
+class IndexListView(ListView):
+    model = Product
+
+# def index(request):
+#     products = Product.objects.all()
+#     context = {'products': products}
+#     return render(request, 'index.html', context)
 
 
 def contacts(request):
@@ -17,6 +21,7 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'Имя -{name}, телефон - {phone}, сообщение - {message}')
     return render(request, 'contacts.html')
+
 
 def info_product(request, pk):
     product = get_object_or_404(Product, pk=pk)
