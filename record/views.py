29f363d685313d0233b00c838dee79a566f8f9aa
groni <smirnovapolina8200@mail.ru>
2024-07-11 03:34:39 +0500

    Сделал счётчик просмотров, сделал фильтр вывода статей, сделал slug для заголовка и после редактирования записи перенаправление на отредактированную страницу

--- record/views.py
+++ record/views.py
@@ -1,5 +1,6 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from record.models import Records
 
@@ -8,10 +9,21 @@ from record.models import Records
 class RecordListView(ListView):
     model = Records
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(sign_of_publication=True)
+        return queryset
+
 
 # Класс-контроллер для детального просмотра записи
 class RecordDetailView(DetailView):
     model = Records
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.number_of_views += 1
+        self.object.save()
+        return self.object
 
 
 # Класс-контроллер для создания записи
@@ -20,12 +32,22 @@ class RecordCreateView(CreateView):
     fields = ('heading', 'content', 'preview',)
     success_url = reverse_lazy('record:record_list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.heading)
+            new_mat.save()
+        return super().form_valid(form)
+
 
 # Класс-контроллер для редактирования записи
 class RecordUpdateView(UpdateView):
     model = Records
     fields = ('heading', 'content', 'preview',)
     success_url = reverse_lazy('record:record_list')
+
+    def get_success_url(self):
+        return reverse('record:view', args=[self.kwargs.get('pk')])
 
 
 # Класс-контроллер для удаления записи
