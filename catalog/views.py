69c4c024024efb0a11e983464e259ca390269c11
groni <smirnovapolina8200@mail.ru>
2024-07-19 18:13:03 +0500

    Всё сделал

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,10 @@
+from django.forms import inlineformset_factory
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
 
 from catalog.forms import ProductForm
-from catalog.models import Product
+from catalog.models import Product, Version
 
 
 # Create your views here.
@@ -11,6 +12,12 @@ from catalog.models import Product
 # Класс-контроллер для вывода главной страницы
 class ProductListView(ListView):
     model = Product
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        a = inlineformset_factory(Product, Version)
+        context_data['formset'] = a()
+        return context_data
 
 
 # Класс-контроллер для создания нового продукта
