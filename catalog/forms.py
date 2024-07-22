dc41bf9b44632913c0c5e290f397b1e759c781c1
groni <groninger.official@yandex.ru>
2024-07-23 00:02:25 +0500

    теперь все формы выводятся в едином стиле

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,16 +1,28 @@
-from django.forms import ModelForm, forms
+from django.forms import ModelForm, forms, BooleanField
 
 from catalog.models import Product, Version
 
 
-# Форма для продукта
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    """Класс для стилизации форм"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = "form-check-input"
+            else:
+                field.widget.attrs['class'] = "form-control"
+
+
+class ProductForm(StyleFormMixin, ModelForm):
+    """Класс форма для продуктов"""
     class Meta:
         model = Product
         exclude = ('created_at', 'updated_at',)
 
-    # Метод для фильтрования слов
     def clean_name(self):
+        """Метод для фильтрования слов"""
         cleaned_data = self.cleaned_data['name']
 
         # Кортеж запрещенных слов
@@ -37,8 +49,8 @@ class ProductForm(ModelForm):
         return cleaned_data
 
 
-# Форма для версий
-class VersionForm(ModelForm):
+class VersionForm(StyleFormMixin, ModelForm):
+    """Класс форма для версий"""
     class Meta:
         model = Version
-        fields = ('version_name',)
+        fields = '__all__'
--- catalog/views.py
+++ catalog/views.py
@@ -15,10 +15,16 @@ class ProductListView(ListView):
     def get_context_data(self, **kwargs):
         """Метод для вывода названия версии если она активна"""
         context_data = super().get_context_data(**kwargs)
-        version = []
-        version.extend(Version.objects.get(product='1'))
-        version_name = version[0].version_name
-        context_data['version'] = version_name
+        version_dict = {}
+        # Запускаю цикл по моделям Product и сразу запускаю цикл по моделям Version, далее алгоритм проверят что модель
+        # Version активная и если он видит что она привязана к продукту, то добавляет в словарь номер продукта и его
+        # активную версию
+        for product in Product.objects.all():
+            for version in Version.objects.all():
+                if version.indicates_current_version:
+                    if version.product_id == int(product.pk):
+                        version_dict[version.product_id] = version.version_name
+        context_data['versions'] = version_dict
         return context_data
 
 
@@ -39,6 +45,25 @@ class ProductUpdateView(UpdateView):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:product_list')
+
+    def get_context_data(self, **kwargs):
+        """Метод для вывода формы версии при редактировании продукта"""
+        context_data = super().get_context_data(**kwargs)
+        ProductFormset = inlineformset_factory(Product, Version, VersionForm, extra=1)
+        if self.request.method == 'POST':
+            context_data['formset'] = ProductFormset(self.request.POST, instance=self.object)
+        else:
+            context_data['formset'] = ProductFormset(instance=self.object)
+        return context_data
+
+    def form_valid(self, form):
+        """Метод для сохранения формы при редактировании"""
+        formset = self.get_context_data()['formset']
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
 
 
 class ProductDeleteView(DeleteView):
