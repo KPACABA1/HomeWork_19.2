f9450f3f815edeaebae0337bdf1c1fb4240c3ecd
groni <groninger.official@yandex.ru>
2024-08-11 03:16:52 +0500

    поправил названия некоторых переменных

--- catalog/services.py
+++ catalog/services.py
@@ -28,15 +28,15 @@ def get_category_from_cache():
     # Если кэш активен, то начинаем работу с ним
     if CACHE_ENABLED:
         key = 'category_list'
-        products_list_cache = cache.get(key)
+        category_list_cache = cache.get(key)
         # Проверяю, получил ли я кэш
-        if products_list_cache is not None:
-            return products_list_cache
+        if category_list_cache is not None:
+            return category_list_cache
         # Если кэш не получен, то записываю его из БД для последующего использования
         else:
-            products = Category.objects.all()
-            cache.set(products, key)
-            return products
+            category = Category.objects.all()
+            cache.set(category, key)
+            return category
     # Если кэш неактивен, то просто возвращаю все продукты
     else:
         return Category.objects.all()
